358c3f4221581e259e2a5ff5fe607583031df6f6
DavidRobertDenis <110538874+DavidRobertDenis@users.noreply.github.com>
2024-01-03 14:06:05 +0200

    Add files via upload

---
+++ Utilizarea comenzilor de control/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 12, 23, -22, -765, 43, 545, -4, -55, 43, 12, 351, -999, -87 };
+
+        // Initializează două liste pentru numerele pozitive și negative
+        System.Collections.Generic.List<int> numerePozitive = new System.Collections.Generic.List<int>();
+        System.Collections.Generic.List<int> numereNegative = new System.Collections.Generic.List<int>();
+
+        // Parcurge fiecare element din șir și le plasează în listele corespunzătoare
+        foreach (int numar in array)
+        {
+            if (numar >= 0)
+            {
+                numerePozitive.Add(numar);
+            }
+            else
+            {
+                numereNegative.Add(numar);
+            }
+        }
+
+        // Afișează numerele pozitive
+        Console.WriteLine("Numerele pozitive sunt:");
+        foreach (int pozitiv in numerePozitive)
+        {
+            Console.Write(pozitiv + " ");
+        }
+
+        // Afișează numerele negative
+        Console.WriteLine("\nNumerele negative sunt:");
+        foreach (int negativ in numereNegative)
+        {
+            Console.Write(negativ + " ");
+        }
+    }
+}
